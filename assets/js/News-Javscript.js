127d531a6366847f7b8c50829d18cead8e4d2c86
JamesHO00 <james27396688@gmail.com>
2022-06-19 05:32:08 +0800

    ver4.0.1

--- assets/js/News-Javscript.js
+++ assets/js/News-Javscript.js
@@ -1,10 +1,12 @@
 const title = document.querySelectorAll('.info__title');
 const showContent = document.querySelectorAll('.info__container');
+const button = document.querySelectorAll('.info__title__button');
 
 let clickFunction = () => {
   title.forEach(function (element, index) {
     element.addEventListener('click', function () {
       showContent[index].classList.toggle('active');
+      button[index].classList.toggle('active');
     });
   });
 };
